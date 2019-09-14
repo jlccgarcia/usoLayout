1f7cae059f724f65a65dbd39c557365730da9091
jlccgarcia <55311434+jlccgarcia@users.noreply.github.com>
2019-09-14 18:16:16 +0200

    Tema 2-3-1-Práctica con diferentes layouts

---
+++ app/src/main/java/com/example/usolayout/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.usolayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_main);
+        //setContentView(R.layout.p1_constraint_layout);// práctica 1
+        //setContentView(R.layout.p2_table_layout);// práctica 2
+        setContentView(R.layout.p3_linear_layout);// práctica 3
+    }
+
+}
